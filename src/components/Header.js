5ce474f4eb474f3f0c0aa1c0d54a40da905788c9
saikrishna-muppa <39872073+saikrishna-muppa@users.noreply.github.com>
2021-06-17 18:44:11 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div style={{ height: "40px", width: "100%", background: "#c84b31" }}>
-      <button style={{ margin: "8px" }}>Logout</button>
+      <button style={{ margin: "8px",marginLeft:"-90%" }}>Logout</button>
     </div>
   );
 };
