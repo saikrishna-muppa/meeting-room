c9a4224cab92c61b400f9fe25f1771e0f876e31e
saikrishna-muppa <39872073+saikrishna-muppa@users.noreply.github.com>
2021-06-17 18:35:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import MeetingRoom from "./components/MeetingRoom";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MeetingRoom />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div style={{ height: "40px", width: "100%", background: "#c84b31" }}>
+      <button style={{ margin: "8px" }}>Logout</button>
+    </div>
+  );
+};
+
+export default Header;
